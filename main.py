02a00f55daf654f3ce277f842ee57d1687f6870e
Dista Reza <dista.reza@bertelsmann.de>
2022-06-07 09:27:47 +0800

    update

--- main.py
+++ main.py
@@ -1,31 +1,71 @@
-from flask import Flask, render_template, request, redirect, url_for, flash
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = "df0331cefc6c2b9a5d0208a726a5d1c0fd37324feba25506"
 
-all_books = []
+##CREATE DATABASE
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///books.db"
+#Optional: But it will silence the deprecation warning in the console.
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+
+##CREATE TABLE
+class Book(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(250), unique=True, nullable=False)
+    author = db.Column(db.String(250), nullable=False)
+    rating = db.Column(db.Float, nullable=False)
+
+
+db.create_all()
+
 
 @app.route('/')
 def home():
-    return render_template("index.html", mylibrary=all_books)
+    ##READ ALL RECORDS
+    all_books = db.session.query(Book).all()
+    return render_template("index.html", books=all_books)
 
 
-@app.route("/add", methods=('GET', 'POST'))
+@app.route("/add", methods=["GET", "POST"])
 def add():
-    if request.method == 'POST':
-        title = request.form['title']
-        author = request.form['author']
-        rating = request.form['rating']
-        if not title:
-            flash('Title is required')
-        elif not author:
-            flash('Author is required')
-        elif not rating:
-            flash('Rating is required')
-        else:
-            all_books.append( { "title" : title , "author" : author, "rating" : rating })
-            return redirect(url_for("home", my_library=all_books))
+    if request.method == "POST":
+        # CREATE RECORD
+        new_book = Book(
+            title=request.form["title"],
+            author=request.form["author"],
+            rating=request.form["rating"]
+        )
+        db.session.add(new_book)
+        db.session.commit()
+        return redirect(url_for('home'))
     return render_template("add.html")
+
+
+@app.route("/edit", methods=["GET", "POST"])
+def edit():
+    if request.method == "POST":
+        #UPDATE RECORD
+        book_id = request.form["id"]
+        book_to_update = Book.query.get(book_id)
+        book_to_update.rating = request.form["rating"]
+        db.session.commit()
+        return redirect(url_for('home'))
+    book_id = request.args.get('id')
+    book_selected = Book.query.get(book_id)
+    return render_template("edit_rating.html", book=book_selected)
+
+
+@app.route("/delete")
+def delete():
+    book_id = request.args.get('id')
+
+    # DELETE A RECORD BY ID
+    book_to_delete = Book.query.get(book_id)
+    db.session.delete(book_to_delete)
+    db.session.commit()
+    return redirect(url_for('home'))
 
 
 if __name__ == "__main__":
